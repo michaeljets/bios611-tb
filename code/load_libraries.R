40ad5744786a099f9180bc15d4cd567c4661927d
Michael Jetsupphasuk <jetsupphasuk@unc.edu>
2020-10-07 16:48:45 -0400

    add R file to load libraries

---
+++ code/load_libraries.R
@@ -0,0 +1,13 @@
+# load_libraries.R
+
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rgeos)
+library(gridExtra)
+library(randomForest)
+library(kableExtra)
+library(stargazer)
+library(tidyverse)
+
+theme_set(theme_bw())
